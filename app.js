c4bb5b0c7d6953f5385b747451134c9b6cf238db
Anubhav Shavaran <anubhavshavaran5678@gmail.com>
2024-09-29 23:07:20 +0530

    #6 Add API endpoint

--- app.js
+++ app.js
@@ -13,15 +13,14 @@ app.use(cors({
 
 app.use(express.json({ limit: '10kb' }));
 
-app.use('/api/verses', versesRouter)
-
-app.get('/api/helloWorld', (req, res, next) => {
+app.use('/', (req, res, next) => {
     res.status(200).json({
         status: 'success',
-        data: 'Hello World!'
+        data: 'This is the API for Selahvie'
     });
 });
 
+app.use('/api/verses', versesRouter);
 
 app.all("*", (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on the server.`, '404'));
